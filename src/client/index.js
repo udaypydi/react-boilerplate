7d792b74b3ab7eb841422c00f0f4016c11f0e990
udaypydi <udaypydi333@gmail.com>
2020-03-28 23:27:44 +0530

    index page added

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './app.js';
 
+// index page rendering root node
+
 render(
   <App />,
   document.getElementById('root')
